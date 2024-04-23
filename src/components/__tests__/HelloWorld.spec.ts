e83fbb707725fe892ad11af56bc615cabefa0fab
이재정 <jj9535@naver.com>
2024-04-23 20:06:24 +0900

    first commit

---
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect } from 'vitest'
+
+// import { mount } from '@vue/test-utils'
+
+// test()
+// it()과 동일
+// expect()의 집합
+
+// expect()
+// 단정문을 사용하기 위한 함수
+
+describe('HelloWorld', () => {
+  it('renders properly', () => {
+    expect('test')
+  })
+})
